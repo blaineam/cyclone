c88eb1c22db001d9f49c19bfc2f8425193b1f08e
Blaine Miller <blaine@blaineam.com>
2023-08-22 08:54:56 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 env >> /etc/environment
 
-(crontab -l ; echo "* * * * * /usr/bin/rclone sync --stats-one-line -P --stats 5s --ignore-existing --log-level NOTICE --ignore-times --low-level-retries 100 --tpslimit 20 --retries 100 --transfers 20 --checkers 20 --bwlimit ${BWLIMIT} \"${SOURCE}\" \"${TARGET}\" > /proc/1/fd/1 2>&1") | crontab -
+(crontab -l ; echo "* * * * * /usr/bin/flock -n /tmp/cyclone.lockfile /usr/bin/rclone sync --stats-one-line -P --stats 5m --ignore-existing --log-level NOTICE --ignore-times --low-level-retries 100 --tpslimit 20 --retries 100 --transfers 20 --checkers 20 --bwlimit ${BWLIMIT} \"${SOURCE}\" \"${TARGET}\" > /proc/1/fd/1 2>&1") | crontab -
 
 # execute CMD
 echo "$@"
