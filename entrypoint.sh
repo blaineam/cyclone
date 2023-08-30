f3f3fc9a078e6baddc652bc59564df894b244023
Blaine Miller <blaine@wemiller.com>
2023-08-30 14:46:46 -0700

    exclude some synology namings

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 env >> /etc/environment
 
-(crontab -l ; echo "* * * * * /usr/bin/flock -n /tmp/cyclone.lockfile /usr/bin/rclone sync --stats-one-line -P --stats 1m --checksum --log-level NOTICE --ignore-times --low-level-retries 100 --tpslimit 20 --retries 100 --transfers 20 --checkers 20 --order-by modtime,desc --check-first --bwlimit ${BWLIMIT} \"${SOURCE}\" \"${TARGET}\" > /proc/1/fd/1 2>&1") | crontab -
+(crontab -l ; echo "* * * * * /usr/bin/flock -n /tmp/cyclone.lockfile /usr/bin/rclone sync --stats-one-line -P --stats 1m --checksum --log-level NOTICE --ignore-times --low-level-retries 100 --tpslimit 20 --retries 100 --transfers 20 --checkers 20 --order-by modtime,desc --check-first --fliter \"- @eaDir\" --fliter \"- *@Syno*\"  --fliter \"- #recycle\" --bwlimit ${BWLIMIT} \"${SOURCE}\" \"${TARGET}\" > /proc/1/fd/1 2>&1") | crontab -
 
 # execute CMD
 echo "$@"
