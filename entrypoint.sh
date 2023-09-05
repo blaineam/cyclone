e5f9a5020edc08214907a8fb5ae759524ca75ae9
Blaine Miller <blaine@wemiller.com>
2023-09-05 08:56:57 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 env >> /etc/environment
 
-echo -e "- @eaDir\n- *@Syno*\n- #recycle" > /filters.txt
+echo -e "- @eaDir/**\n- *@Syno*\n- #recycle/**" > /filters.txt
 
 (crontab -l ; echo "* * * * * /usr/bin/flock -n /tmp/cyclone.lockfile /usr/bin/rclone sync --stats-one-line -P --stats 1m --size-only --log-level NOTICE --low-level-retries 100 --tpslimit 20 --retries 100 --transfers 20 --checkers 20 --order-by modtime,desc --check-first --filter-from /filters.txt --bwlimit ${BWLIMIT} \"${SOURCE}\" \"${TARGET}\" > /proc/1/fd/1 2>&1") | crontab -
 
