f9495fc8ef984709fb953c639903bb03a327e9f6
Blaine Miller <blaine@wemiller.com>
2024-01-24 23:32:07 -0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,7 @@ env >> /etc/environment
 
 echo -e "- @eaDir/**\n- *@Syno*\n- #recycle/**" > /filters.txt
 
-(crontab -l ; echo "* * * * * /usr/bin/flock -n /tmp/cyclone.lockfile /usr/bin/rclone ${PLAN} --stats 1m --size-only --log-level ERROR --log-file=/proc/1/fd/1 --low-level-retries 100 --tpslimit 20 --retries 100 --transfers 20 --checkers 20 --order-by modtime,desc --check-first --filter-from /filters.txt --bwlimit ${BWLIMIT} ${EXTRAFLAGS} \"${SOURCE}\" \"${TARGET}\"") | crontab -
-
+(crontab -l ; echo "* * * * * /usr/bin/flock -n /tmp/cyclone.lockfile /usr/bin/rclone ${PLAN} --stats 1m --size-only --log-level NOTICE --stats-one-line -P --low-level-retries 100 --tpslimit 20 --retries 100 --transfers 20 --checkers 20 --order-by modtime,desc --check-first --filter-from /filters.txt --bwlimit ${BWLIMIT} ${EXTRAFLAGS} \"${SOURCE}\" \"${TARGET}\" > /proc/1/fd/1") | crontab -
 # execute CMD
 echo "$@"
 exec "$@"
