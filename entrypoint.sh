a8682527013c8df0e28fa42fcf3d8e80cc3d25c1
Blaine Miller <blaine@wemiller.com>
2024-01-24 20:24:11 -0800

    improve logging

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ env >> /etc/environment
 
 echo -e "- @eaDir/**\n- *@Syno*\n- #recycle/**" > /filters.txt
 
-(crontab -l ; echo "* * * * * /usr/bin/flock -n /tmp/cyclone.lockfile /usr/bin/rclone ${PLAN} --stats-one-line -P --stats 1m --size-only --log-level ERROR --low-level-retries 100 --tpslimit 20 --retries 100 --transfers 20 --checkers 20 --order-by modtime,desc --check-first --filter-from /filters.txt --bwlimit ${BWLIMIT} ${EXTRAFLAGS} \"${SOURCE}\" \"${TARGET}\" > /proc/1/fd/1 2>&1") | crontab -
+(crontab -l ; echo "* * * * * /usr/bin/flock -n /tmp/cyclone.lockfile /usr/bin/rclone ${PLAN} --stats 1m --size-only --log-level ERROR --log-file=/proc/1/fd/1 --low-level-retries 100 --tpslimit 20 --retries 100 --transfers 20 --checkers 20 --order-by modtime,desc --check-first --filter-from /filters.txt --bwlimit ${BWLIMIT} ${EXTRAFLAGS} \"${SOURCE}\" \"${TARGET}\"") | crontab -
 
 # execute CMD
 echo "$@"
